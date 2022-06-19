1ae2c867af471d84403e0c99576295e7a33e4d39
mastershark <frochbg@gmail.com>
2022-06-19 15:13:30 +0300

    Added ConsoleEngine's constructor

--- src/UI/Console/ConsoleEngine.java
+++ src/UI/Console/ConsoleEngine.java
@@ -1,6 +1,10 @@
 package UI.Console;
 
 public final class ConsoleEngine {
+    private ConsoleEngine() throws InstantiationException {
+        throw new InstantiationException("Cannot instantiate ConsoleEngline class!");
+    }
+
     public static void run() {
         System.out.println("hello world!");
     }
